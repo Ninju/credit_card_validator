b79e5fac1dad2eee4e7473f09fadf134b3265de7
Alex Watt <alex.watt@me.com>
2010-11-03 00:47:36 +0000

    forgot even? and odd? are already defined

--- lib/credit_card_validator/fixnum.rb
+++ lib/credit_card_validator/fixnum.rb
@@ -2,12 +2,4 @@ Fixnum.class_eval do
   def digits
     to_s.split( // ).map { | s | s.to_i }
   end
-
-  def even?
-    self % 2 == 0
-  end
-
-  def odd?
-    not even?
-  end
 end
--- spec/credit_card_validator/fixnum_spec.rb
+++ spec/credit_card_validator/fixnum_spec.rb
@@ -7,21 +7,4 @@ describe Fixnum do
   it "should return the digits as an array" do
     125.digits.should == [ 1, 2, 5 ]
   end
-
-  it "should be even if it is divisible exactly by 2" do
-    100.should be_even
-  end
-
-  it "should not be even if it is not divisible exactly by 2" do
-    99.should_not be_even
-  end
-
-  it "should be odd if it is not divisible exactly by 2" do
-    37.should be_odd
-  end
-
-  it "should not be odd if it is divisible exactly by 2" do
-    2.should_not be_odd
-  end
-
 end
